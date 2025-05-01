50b9697da4480a2ffdd4d997611e7e6f746ba394
jeonghoon11 <jih361@naver.com>
2025-05-01 22:57:28 +0900

    [feat] 메인 구현

--- week3/assignment/src/App.jsx
+++ week3/assignment/src/App.jsx
@@ -1,10 +1,26 @@
 import "./styles/App.css";
 import Header from "./components/Header/Header";
+import { useState } from "react";
+import { menus } from "./components/menu";
+import GitFind from "./components/Body/GitFind/GitFind";
+import NumberBase from "./components/Body/NumberBase/NumberBase";
 
 function App() {
+  const [selectedMenu, setSelectedMenu] = useState("github");
+  const selectedMenuItem = menus.find((menu) => menu.key === selectedMenu);
+
   return (
     <div>
-      <Header />
+      <Header
+        selectedMenu={selectedMenu}
+        setSelectedMenu={setSelectedMenu}
+        menus={menus}
+      />
+      {selectedMenu === "github" ? (
+        <GitFind content={selectedMenuItem.content} />
+      ) : (
+        <NumberBase content={selectedMenuItem.content} />
+      )}
     </div>
   );
 }
